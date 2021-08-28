e00863ec36328e91e7a1144f2bddfdb2c539c38c
fsociety <fsociety@root>
2021-08-28 14:53:09 +0530

    new changes

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Testimonials from "./Components/Testimonials";
+import Portfolio from "./Components/Portfolio";
+
+const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Resume data={resumeData.resume} />
+      <Portfolio data={resumeData.portfolio} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Resume.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+const Resume = ({ data }) => {
+  if (data) {
+    var skillmessage = data.skillmessage;
+    var education = data.education.map(function (education) {
+      return (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree}
+            <br />
+            <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+          <p>{education.description}</p>
+        </div>
+      );
+    });
+    var work = data.work.map(function (work) {
+      return (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <br />
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p className="description">{work.description}</p>
+        </div>
+      );
+    });
+    var skills = data.skills.map(function (skills) {
+      return (
+        <div className="skill__container" key={skills.name}>
+          <em className="skill__name">{skills.name}</em>
+          <img alt={skills.name} className="skill__image" src={skills.img} />
+        </div>
+      );
+    });
+  }
+
+  return (
+    <section id="resume">
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">{education}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Work</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">{work}</div>
+      </div>
+
+      <div className="row skill">
+        <div className="three columns header-col">
+          <h1>
+            <span>Skills</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <p>{skillmessage}</p>
+
+          <div className="bars">
+            <div className="skills">{skills}</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
